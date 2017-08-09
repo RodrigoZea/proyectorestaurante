56708a0ae34b9b1dff9fea7e55abd25e5844fad3
Rodrigo Zea <zearodrigo37@gmail.com>
2017-08-08 20:24:31 -0600

    actualizando el repositorio

---
+++ src/users.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MIGUEL
+ */
+public class users {
+    
+private String usuario, clave;
+    
+    public users(){
+        this.usuario = "";
+        this.clave = "";
+    }
+    
+    public users(String usuario, String clave){ 
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+    
+    
+}
